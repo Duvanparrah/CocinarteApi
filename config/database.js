e0898f13a96cd51c26dc561d8ff0ab8cbe1fec0e
mebel <mebelcampo394@gamil.com>
2025-05-20 09:08:58 -0500

    actualizacionadministrador

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASSWORD, // Contraseña
   {
     host: process.env.MYSQL_HOST, // Dirección del servidor de base de datos
+    port: process.env.MYSQL_PORT,
     dialect: 'mysql', // Usamos MySQL
     logging: false, // Puedes poner true si quieres ver las consultas SQL
     dialectOptions: {
